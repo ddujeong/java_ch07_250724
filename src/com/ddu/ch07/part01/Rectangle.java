6ccef0c594531c0d1d568699c7d990d77275c46f
ddujeong <qoddl159@nate.com>
2025-07-24 16:08:26 +0900

    상속 코드

---
+++ src/com/ddu/ch07/part01/Rectangle.java
@@ -0,0 +1,8 @@
+package com.ddu.ch07.part01;
+
+public class Rectangle extends Shape { // 부모 클래스인 Shape 클래스로 부터 상속받는 자식클래스 Rectangle 선언
+	
+	 // 다중상속 xxxx 안됨
+	 // 자식 클래스 extends 부모 클래스
+	 	int recArea;
+}
---
+++ src/com/ddu/ch07/part01/Shape.java
@@ -0,0 +1,14 @@
+package com.ddu.ch07.part01;
+
+public class Shape { // 부모 클래스
+	int width;
+	int height;
+	String color; // 도형면의 색깔
+	
+	public int area() { // 도형의 면적
+		int area = width* height;
+		return area;
+	}
+	
+	
+}
